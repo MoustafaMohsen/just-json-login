c4067d45cf99f28e136c7b579374139a672f65b6
MoustafaMohsen <moustafamohsen1@gmail.com>
2021-05-26 12:42:42 +0200

    added cors header changed authorize header

--- just_json_login.php
+++ just_json_login.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Just JSON login
- * Description: Authentication for JSON API that's simple, use header "A-Authentication: Basic base64(username:password)"
+ * Description: Authentication for JSON API that's simple, use header "TTT: Basic base64(username:password)", or normal authentication header
  * Author URI: https://github.com/MoustafaMohsen
  * Version: 0.1
  * Plugin URI: https://github.com/MoustafaMohsen/
@@ -23,7 +23,7 @@ function json_basic_auth_handler( $user ) {
         $username = $_SERVER['PHP_AUTH_USER'];
         $password = $_SERVER['PHP_AUTH_PW'];
 	}else{
-		$http = 'A_Authorization';
+		$http = 'TTT';
 		$http = strtoupper($http);
 		$header = $_SERVER['HTTP_'.$http] ?? $_SERVER[$http] ?? NULL;
 		preg_match('/Basic (.+)/', $header, $matchArr);
@@ -31,15 +31,16 @@ function json_basic_auth_handler( $user ) {
 		preg_match('/(.*):(.*)/', $decoded, $autArr);
 		$username = $autArr[1];
 		$password = $autArr[2];
-	
-	
 		if(is_null($header)){
 			
 			return null;
 		}
-	
 	}
-	remove_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+
+
+
+
+    remove_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
 	$user = wp_authenticate( $username, $password );
 
@@ -51,9 +52,8 @@ function json_basic_auth_handler( $user ) {
 	}
 
 	$wp_json_basic_auth_error = true;
+
 	return $user->ID;
-
-
 }
 add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
@@ -68,3 +68,15 @@ function json_basic_auth_error( $error ) {
 	return $wp_json_basic_auth_error;
 }
 add_filter( 'rest_authentication_errors', 'json_basic_auth_error' );
+
+
+function my_customize_rest_cors() {
+	add_action( 'rest_pre_serve_request', function () {
+		header( "Access-Control-Allow-Origin:  *" );
+		header( "Access-Control-Allow-Methods: GET, POST, OPTIONS" );
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		header( "Access-Control-Allow-Credentials: true" );
+	});
+}
+
+add_action( 'rest_api_init', 'my_customize_rest_cors', 15 );
